858c8f98c33d7c7fa612eb9ac83f95fe4e8eb0ae
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2024-09-15 01:41:54 +0400

    fix(tests): Increase timeout [skip-release] (#2)

--- testing/e2e.test.ts
+++ testing/e2e.test.ts
@@ -18,87 +18,107 @@ describe('e2e', () => {
     });
 
     describe('downloadMasterDatabase', () => {
-        it('should get the latest master metadata and then download it and populate our master database', async () => {
-            const dbPath = path.join(outputDir, `master.db`);
+        it(
+            'should get the latest master metadata and then download it and populate our master database',
+            async () => {
+                const dbPath = path.join(outputDir, `master.db`);
 
-            const result = await downloadMasterDatabase({ outputFile: { path: dbPath } });
-            expect(result).toEqual(dbPath);
+                const result = await downloadMasterDatabase({ outputFile: { path: dbPath } });
+                expect(result).toEqual(dbPath);
 
-            const client: Client = createClient({
-                url: `file:${dbPath}`,
-            });
+                const client: Client = createClient({
+                    url: `file:${dbPath}`,
+                });
 
-            try {
-                const {
-                    rows: [{ authors, books, categories }],
-                } = await client.execute(
-                    'SELECT (SELECT COUNT(*) FROM authors) AS authors, (SELECT COUNT(*) FROM books) AS books, (SELECT COUNT(*) FROM categories) AS categories',
-                );
+                try {
+                    const {
+                        rows: [{ authors, books, categories }],
+                    } = await client.execute(
+                        'SELECT (SELECT COUNT(*) FROM authors) AS authors, (SELECT COUNT(*) FROM books) AS books, (SELECT COUNT(*) FROM categories) AS categories',
+                    );
 
-                expect((authors as number) > 3000).toBe(true);
-                expect((books as number) > 8000).toBe(true);
-                expect((categories as number) > 30).toBe(true);
-            } finally {
-                client.close();
-            }
-        });
+                    expect((authors as number) > 3000).toBe(true);
+                    expect((books as number) > 8000).toBe(true);
+                    expect((categories as number) > 30).toBe(true);
+                } finally {
+                    client.close();
+                }
+            },
+            { timeout: 20000 },
+        );
 
-        it('should get the latest master metadata and then download it as a JSON', async () => {
-            const dbPath = path.join(outputDir, `master.json`);
+        it(
+            'should get the latest master metadata and then download it as a JSON',
+            async () => {
+                const dbPath = path.join(outputDir, `master.json`);
 
-            const result = await downloadMasterDatabase({ outputFile: { path: dbPath } });
-            expect(result).toEqual(dbPath);
+                const result = await downloadMasterDatabase({ outputFile: { path: dbPath } });
+                expect(result).toEqual(dbPath);
 
-            const { authors, books, categories } = JSON.parse(await fs.readFile(result, 'utf8'));
+                const { authors, books, categories } = JSON.parse(await fs.readFile(result, 'utf8'));
 
-            expect((authors.length as number) > 3000).toBe(true);
-            expect((books.length as number) > 8000).toBe(true);
-            expect((categories.length as number) > 30).toBe(true);
-        });
+                expect((authors.length as number) > 3000).toBe(true);
+                expect((books.length as number) > 8000).toBe(true);
+                expect((categories.length as number) > 30).toBe(true);
+            },
+            { timeout: 20000 },
+        );
     });
 
     describe('downloadBook', () => {
-        it('should get the books major version url then download it', async () => {
-            const dbPath = path.join(outputDir, `book.db`);
+        it(
+            'should get the books major version url then download it',
+            async () => {
+                const dbPath = path.join(outputDir, `book.db`);
 
-            const result = await downloadBook(26592, { outputFile: { path: dbPath } });
-            expect(result).toEqual(dbPath);
+                const result = await downloadBook(26592, { outputFile: { path: dbPath } });
+                expect(result).toEqual(dbPath);
 
-            const client: Client = createClient({
-                url: `file:${dbPath}`,
-            });
+                const client: Client = createClient({
+                    url: `file:${dbPath}`,
+                });
 
-            try {
-                const {
-                    rows: [{ pages, titles }],
-                } = await client.execute(
-                    'SELECT (SELECT COUNT(*) FROM page) AS pages, (SELECT COUNT(*) FROM title) AS titles',
-                );
+                try {
+                    const {
+                        rows: [{ pages, titles }],
+                    } = await client.execute(
+                        'SELECT (SELECT COUNT(*) FROM page) AS pages, (SELECT COUNT(*) FROM title) AS titles',
+                    );
 
-                expect((pages as number) > 90).toBe(true);
-                expect((titles as number) > 0).toBe(true);
-            } finally {
-                client.close();
-            }
-        });
+                    expect((pages as number) > 90).toBe(true);
+                    expect((titles as number) > 0).toBe(true);
+                } finally {
+                    client.close();
+                }
+            },
+            { timeout: 20000 },
+        );
 
-        it('should get the books major version url then download it as json', async () => {
-            const dbPath = path.join(outputDir, `book.json`);
+        it(
+            'should get the books major version url then download it as json',
+            async () => {
+                const dbPath = path.join(outputDir, `book.json`);
 
-            const result = await downloadBook(26592, { outputFile: { path: dbPath } });
-            expect(result).toEqual(dbPath);
+                const result = await downloadBook(26592, { outputFile: { path: dbPath } });
+                expect(result).toEqual(dbPath);
 
-            const { pages, titles } = JSON.parse(await fs.readFile(result, 'utf8'));
+                const { pages, titles } = JSON.parse(await fs.readFile(result, 'utf8'));
 
-            expect(pages.length > 90).toBe(true);
-            expect(titles.length > 0).toBe(true);
-        });
+                expect(pages.length > 90).toBe(true);
+                expect(titles.length > 0).toBe(true);
+            },
+            { timeout: 20000 },
+        );
 
-        it('should get the books major version url then download it as a typed object', async () => {
-            const { pages, titles = [] } = await getBook(26592);
+        it(
+            'should get the books major version url then download it as a typed object',
+            async () => {
+                const { pages, titles = [] } = await getBook(26592);
 
-            expect(pages.length > 90).toBe(true);
-            expect(titles.length > 0).toBe(true);
-        });
+                expect(pages.length > 90).toBe(true);
+                expect(titles.length > 0).toBe(true);
+            },
+            { timeout: 20000 },
+        );
     });
 });
