e03e580ce1e48a907a2f12eb7ce37f7a4c22ae91
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2024-09-14 22:34:54 +0400

    Update type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,3 @@
-import { Book } from './book';
-
 export type GetMasterMetadataResponsePayload = {
     url: string;
     version: number;
@@ -41,6 +39,48 @@ export type Author = {
     death?: number;
     id: number;
     name: string;
+};
+
+type PDFFile = {
+    file: string;
+    id?: string;
+};
+
+export type PDFLinks = {
+    alias?: number;
+    cover?: number;
+    cover_alias?: number;
+    files?: PDFFile[];
+    root?: string;
+    size?: number;
+};
+
+export type Metadata = {
+    coauthor?: number[];
+    date: string;
+    group?: number;
+    hide_diacritic?: boolean;
+    min_ver?: number;
+    prefix?: string;
+    shorts: Record<string, string>;
+    sub_books: number[];
+    suffix?: string;
+};
+
+export type Book = {
+    author: number | number[];
+    bibliography: string;
+    category: number;
+    date?: number;
+    hint?: string;
+    id: number;
+    major: number;
+    metadata: Metadata;
+    minor?: number;
+    name: string;
+    pdfLinks?: PDFLinks;
+    printed: number;
+    type: number;
 };
 
 export type Category = {
