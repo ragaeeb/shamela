0b16a5d34016c49e15b2cf58a0c6a41c7f8eff54
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2024-10-10 23:17:16 +0400

    fix(api): If a database does not have is_deleted field library crashes #5 (#6)
    Also expose type definitions

--- src/db/common.ts
+++ src/db/common.ts
@@ -1,5 +1,15 @@
 import { Client, Row } from '@libsql/client';
 
+export type InternalTable = {
+    name: string;
+};
+
+export const getInternalTables = async (db: Client, dbName: string): Promise<InternalTable[]> => {
+    const { rows: tables } = await db.execute(`SELECT name FROM ${dbName}.sqlite_master WHERE type='table'`);
+
+    return tables as unknown as InternalTable[];
+};
+
 export const selectAllRows = async (client: Client, table: string): Promise<Row[]> => {
     const { rows } = await client.execute(`SELECT * FROM ${table}`);
     return rows;
